7f3507b0ab35b1d7af9c13d7e547506f69d22087
ddeng50101 <ddeng@chicagobooth.edu>
2018-01-17 20:46:09 -0600

    Initial Commit

---
+++ food tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  food tracker
+//
+//  Created by Doris Deng on 1/17/18.
+//  Copyright © 2018 Doris Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
